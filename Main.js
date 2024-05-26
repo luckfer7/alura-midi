26f2e0529ed6d3933ae36ca57bfb52413b0720d0
luckfer7 <158299186+luckfer7@users.noreply.github.com>
2024-05-25 21:09:02 -0300

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,46 @@
+
+function tocarSom (selectorAudio) {
+    const elemento = document.querySelector (selectorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    }
+
+    else {
+        console.log ('Elemento não encontrado ou seletor inválido');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+    console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocarSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code == "Space")
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
